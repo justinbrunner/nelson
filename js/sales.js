df600319fc2a6a730c859c72e413a4bc9a3d53e7
Justin <justin@cruxstudios.ca>
2016-03-18 20:08:09 -0400

    updated JS file

--- js/sales.js
+++ js/sales.js
@@ -2,7 +2,27 @@
 (function() {
 	"use strict";
 	
-	var priceTri = document.querySelector("#price_tri"), priceTra = document.querySelector("#price_tra"), loadTri = document.querySelector("#load_tri"), dumpTri = document.querySelector("#dump_tri"), loadTra = document.querySelector("#load_tra"), dumpTra = document.querySelector("#dump_tra"), avgTri = document.querySelector("#avg_tri"), avgTra = document.querySelector("#avg_tra"), triTotal = document.querySelector("#triTotal"), traTotal = document.querySelector("#traTotal"), google1way = document.querySelector("#googleOneWay");
+	var priceTri = document.querySelector("#price_tri"),
+		priceTra = document.querySelector("#price_tra"), 
+		loadTri = document.querySelector("#load_tri"),
+		dumpTri = document.querySelector("#dump_tri"), 
+		loadTra = document.querySelector("#load_tra"), 
+		dumpTra = document.querySelector("#dump_tra"), 
+		avgTri = document.querySelector("#avg_tri"),
+		avgTra = document.querySelector("#avg_tra"), 
+		triTotal = document.querySelector("#triTotal"),
+		traTotal = document.querySelector("#traTotal"),
+		googleTimeTri = document.querySelector("#googleTimeTri"),
+		googleTimeTra = document.querySelector("#googleTimeTra"),
+		googleTriInc = document.querySelector("#googleTimeTriInc"),
+		googleTraInc = document.querySelector("#googleTimeTraInc"),
+		tcTime_tri = document.querySelector("#tcTime_tri"),
+		tcTime_tra = document.querySelector("#tcTime_tra"),
+		wTime = document.querySelector("#wTime"),
+		trips_tri_val = document.querySelect("#trips_tri"),
+		tripsRounded_tri_val = document.querySelect("#tripsRounded_tri"),
+		trips_tra_val = document.querySelect("#trips_tra"),
+		tripsRounded_tra_val = document.querySelect("#tripsRounded_tra");
 	
 	
 	
@@ -10,14 +30,14 @@
 	var triAxlePrice = 0; //B3
 	var loadTime_ta = 0; //B4
 	var dumpTime_ta = 0; //B5
-	var Total_tri_load_dump = 0; //B6
+	var total_tri_load_dump = 0; //B6
 	var avgWeight_tri = 0; //B8
 	
 	// Trailer
 	var trailerPrice = 0; //C3
 	var loadTime_tr = 0; //C4
 	var dumpTime_tr = 0; //C5
-	var Total_trailer_load_dump = 0; //C6
+	var total_trailer_load_dump = 0; //C6
 	var avgWeight_tra = 0; //C8
 	
 	// Trip Time
@@ -28,17 +48,16 @@
 	var perIncrease_tra = (roundTrip_tra * 1.10).toFixed(1); // C11
 	
 	// Total Time
-	var totalCycleTime_tri = parseFloat(Total_tri_load_dump) + parseFloat(perIncrease_tri); //B12
-	var totalCycleTime_tra = parseFloat(Total_trailer_load_dump) + parseFloat(perIncrease_tra); //C12
-	//console.log(totalCycleTime_tri);
-	//console.log(totalCycleTime_tra);
+	var totalCycleTime_tri; //B12
+	var totalCycleTime_tra; //C12
 	
 	//Trips per 10 hour day //B&C 13
-	var trips_tri = (600/totalCycleTime_tri).toFixed(2);
-	var trips_tra = (600/totalCycleTime_tra).toFixed(2);
+	var workingDay = 600;
+	var trips_tri;
+	var trips_tra;
 	//Rounded trip values B&C 14
-	var trips_tri_rounded = Math.floor(600/totalCycleTime_tri).toFixed(1); 
-	var trips_tra_rounded = Math.floor(600/totalCycleTime_tra).toFixed(1);
+	var trips_tri_rounded; 
+	var trips_tra_rounded;
 	
 	//Tri-Axle LOW
 	var rate_tri_low = (totalCycleTime_tri * triAxlePrice/60/avgWeight_tri).toFixed(1); //B17
@@ -62,9 +81,13 @@
 	
 	function selectOption(evt) {
 		var target = evt.target.id;
-		var selectValue = this.value;
+		var selectValue = evt.currentTarget.value;
 		
 		switch(target) {
+			case "wTime":
+				workingDay = selectValue;
+				console.log("Tri-axle: "+workingDay);
+				break;
 			case "price_tri":
 				triAxlePrice = selectValue;
 				console.log("Tri-axle: "+triAxlePrice);
@@ -97,21 +120,61 @@
 				avgWeight_tra = selectValue;
 				console.log("Average Trailer Weight: "+avgWeight_tra);
 				break;
-		}
+		}	
 		
-		if(loadTime_ta > 0 && dumpTime_ta > 0) {
-			Total_tri_load_dump = parseInt(loadTime_ta)+parseInt(dumpTime_ta);
-			triTotal.innerHTML = "Total Load and Unload Time: "+Total_tri_load_dump;
-			console.log("Load/Dump - Tri-axle: "+Total_tri_load_dump);
-		}
+		//TIME
+		if(loadTime_ta > 0 && dumpTime_ta > 0) { buildTime("tri"); }
+		if(loadTime_tr > 0 && dumpTime_tr > 0) { buildTime("tra"); }
 		
-		if(loadTime_tr > 0 && dumpTime_tr > 0) {
-			Total_trailer_load_dump = parseInt(loadTime_tr)+parseInt(dumpTime_tr);
-			traTotal.innerHTML = "Total Load and Unload Time: "+Total_trailer_load_dump;
-			console.log("Load/Dump - Trailer: "+Total_trailer_load_dump);
+	}
+	
+	function buildTime(truckType) {
+		
+		if(truckType === "tri") {
+			total_tri_load_dump = parseInt(loadTime_ta)+parseInt(dumpTime_ta);
+			triTotal.innerHTML = "Total Load and Unload Time: "+total_tri_load_dump;
+			totalCycleTime_tri = parseFloat(total_tri_load_dump) + parseFloat(perIncrease_tri); //B12
+			console.log(totalCycleTime_tri);
+			trips_tri = (workingDay/totalCycleTime_tri).toFixed(2);
+			trips_tri_val.innerHTML = trips_tri;
+			//Rounded trip values B&C 14
+			trips_tri_rounded = Math.floor(workingDay/totalCycleTime_tri).toFixed(1);
+			tripsRounded_tri_val.innerHTML = trips_tri_rounded;
+			//console.log("Load/Dump - Tri-axle: "+total_tri_load_dump);
+			tcTime_tri.innerHTML = totalCycleTime_tri;
+			//Round Trip
+			if(roundTrip_tri > 0) { roundTrip("tri"); }
+
+		}else{
+			total_trailer_load_dump = parseInt(loadTime_tr)+parseInt(dumpTime_tr);
+			traTotal.innerHTML = "Total Load and Unload Time: "+total_trailer_load_dump;
+			totalCycleTime_tra = parseFloat(total_trailer_load_dump) + parseFloat(perIncrease_tra); 
+			console.log(totalCycleTime_tra);
+			trips_tra = (workingDay/totalCycleTime_tra).toFixed(2);
+			trips_tra_val.innerHTML = trips_tra;
+			//Rounded trip values B&C 14
+			trips_tra_rounded = Math.floor(workingDay/totalCycleTime_tra).toFixed(1);
+			tripsRounded_tra_val.innerHTML = trips_tra_rounded;
+			//console.log("Load/Dump - Trailer: "+total_trailer_load_dump);
+			tcTime_tra.innerHTML = totalCycleTime_tra;
+			if(roundTrip_tra > 0) { roundTrip("tra"); }
 		}
 	}
 	
+	function roundTrip(type) {
+		//builds round trip time
+		if(type === "tri") {
+			googleTimeTri.innerHTML = roundTrip_tri;
+			googleTriInc.innerHTML = perIncrease_tri;
+			
+		}else{
+			googleTimeTra.innerHTML = roundTrip_tra;
+			googleTraInc.innerHTML = perIncrease_tra;
+		}
+	}
+	
+	
+	wTime.addEventListener("change", selectOption, false);
 	priceTri.addEventListener("change", selectOption, false);
 	priceTra.addEventListener("change", selectOption, false);
 	loadTri.addEventListener("change", selectOption, false);
