21da12045365127d28dc6206f0d779bb0cc675c7
justinbrunner <justinbrunner@users.noreply.github.com>
2016-05-01 12:27:13 -0400

    update

--- js/layout.js
+++ js/layout.js
@@ -1,11 +1,12 @@
 // JavaScript Document
 (function() {
 	"use strict";
-	var sw = document.querySelector(".shadow");
+	var sw = document.querySelector(".shadow"), map = document.querySelector("#map_canvas");
 	
 	function gwh() {
 		var vh = window.innerHeight;
-		var sw.style.height = vh+"px";	
+		sw.style.height = vh+"px";
+		map.style.height = (vh-102)+"px";	
 	}
 	
 	window.addEventListener("load", gwh, false); 
