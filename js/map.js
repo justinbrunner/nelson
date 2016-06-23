a734bec5340f9dcc793cdddfcec880e7a27a92e2
justinbrunner <justinbrunner@users.noreply.github.com>
2016-06-23 14:58:39 -0400

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -7,15 +7,26 @@
         mb = document.querySelector("#mb"),
         dt = document.querySelector("#dt"),
 		lrgMp = document.querySelector("#lrg_map"),
+		close_map = document.querySelector("#closeMap"),
 		mapLg = document.querySelector("#lrgMap");
 
 	function largeMap() {
 		mapLg.style.display = "block";
 		mapLg.appendChild(mapParent);
 		createRoute();
+		google.maps.event.trigger(map, 'resize');
+	}
+	
+	function removeLrg() {
+		//mapLg.removeChild(mapParent);
+		mapLg.style.display = "none";
+		moveMap(mq)
+		createRoute();
+		google.maps.event.trigger(map, 'resize');
 	}
 	
 	lrgMp.addEventListener("click", largeMap, false);
+	close_map.addEventListener("click", removeLrg, false);
 
     function moveMap(mq) {
         if (mq.matches) {
